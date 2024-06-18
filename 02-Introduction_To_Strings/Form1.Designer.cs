3ca5c4a748acb8ab3d57a94bfe990407e02ad1a3
Leila Noval <109560883+lenovalsha@users.noreply.github.com>
2024-06-17 19:42:33 -0600

    Add project files.

---
+++ 02-Introduction_To_Strings/Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace _02_Introduction_To_Strings
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.btnLength = new System.Windows.Forms.Button();
+            this.btnSubstring4 = new System.Windows.Forms.Button();
+            this.btnSubstring35 = new System.Windows.Forms.Button();
+            this.btnStartsWith = new System.Windows.Forms.Button();
+            this.btnEndsWith = new System.Windows.Forms.Button();
+            this.btnIndexof = new System.Windows.Forms.Button();
+            this.btnToUpper = new System.Windows.Forms.Button();
+            this.btnToLower = new System.Windows.Forms.Button();
+            this.btnReplace = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnRegular = new System.Windows.Forms.Button();
+            this.lstOutput = new System.Windows.Forms.ListBox();
+            this.btnReverse = new System.Windows.Forms.Button();
+            this.btnLetter = new System.Windows.Forms.Button();
+            this.btnProblem1 = new System.Windows.Forms.Button();
+            this.btnProblem2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(75, 12);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(210, 20);
+            this.txtInput.TabIndex = 0;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(75, 179);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(210, 20);
+            this.txtOutput.TabIndex = 1;
+            // 
+            // btnLength
+            // 
+            this.btnLength.Location = new System.Drawing.Point(16, 38);
+            this.btnLength.Name = "btnLength";
+            this.btnLength.Size = new System.Drawing.Size(110, 41);
+            this.btnLength.TabIndex = 2;
+            this.btnLength.Text = ".Length";
+            this.btnLength.UseVisualStyleBackColor = true;
+            this.btnLength.Click += new System.EventHandler(this.btnLength_Click);
+            // 
+            // btnSubstring4
+            // 
+            this.btnSubstring4.Location = new System.Drawing.Point(132, 38);
+            this.btnSubstring4.Name = "btnSubstring4";
+            this.btnSubstring4.Size = new System.Drawing.Size(110, 41);
+            this.btnSubstring4.TabIndex = 3;
+            this.btnSubstring4.Text = ".Substring(4)";
+            this.btnSubstring4.UseVisualStyleBackColor = true;
+            this.btnSubstring4.Click += new System.EventHandler(this.btnSubstring4_Click);
+            // 
+            // btnSubstring35
+            // 
+            this.btnSubstring35.Location = new System.Drawing.Point(248, 38);
+            this.btnSubstring35.Name = "btnSubstring35";
+            this.btnSubstring35.Size = new System.Drawing.Size(110, 41);
+            this.btnSubstring35.TabIndex = 4;
+            this.btnSubstring35.Text = ".Substring(3,5)";
+            this.btnSubstring35.UseVisualStyleBackColor = true;
+            this.btnSubstring35.Click += new System.EventHandler(this.btnSubstring35_Click);
+            // 
+            // btnStartsWith
+            // 
+            this.btnStartsWith.Location = new System.Drawing.Point(16, 85);
+            this.btnStartsWith.Name = "btnStartsWith";
+            this.btnStartsWith.Size = new System.Drawing.Size(110, 41);
+            this.btnStartsWith.TabIndex = 5;
+            this.btnStartsWith.Text = ".StartsWith";
+            this.btnStartsWith.UseVisualStyleBackColor = true;
+            this.btnStartsWith.Click += new System.EventHandler(this.btnStartsWith_Click);
+            // 
+            // btnEndsWith
+            // 
+            this.btnEndsWith.Location = new System.Drawing.Point(132, 85);
+            this.btnEndsWith.Name = "btnEndsWith";
+            this.btnEndsWith.Size = new System.Drawing.Size(110, 41);
+            this.btnEndsWith.TabIndex = 6;
+            this.btnEndsWith.Text = ".EndsWith";
+            this.btnEndsWith.UseVisualStyleBackColor = true;
+            // 
+            // btnIndexof
+            // 
+            this.btnIndexof.Location = new System.Drawing.Point(248, 85);
+            this.btnIndexof.Name = "btnIndexof";
+            this.btnIndexof.Size = new System.Drawing.Size(110, 41);
+            this.btnIndexof.TabIndex = 7;
+            this.btnIndexof.Text = ".IndexOf";
+            this.btnIndexof.UseVisualStyleBackColor = true;
+            this.btnIndexof.Click += new System.EventHandler(this.btnIndexof_Click);
+            // 
+            // btnToUpper
+            // 
+            this.btnToUpper.Location = new System.Drawing.Point(16, 132);
+            this.btnToUpper.Name = "btnToUpper";
+            this.btnToUpper.Size = new System.Drawing.Size(110, 41);
+            this.btnToUpper.TabIndex = 8;
+            this.btnToUpper.Text = ".ToUpper()";
+            this.btnToUpper.UseVisualStyleBackColor = true;
+            this.btnToUpper.Click += new System.EventHandler(this.btnToUpper_Click);
+            // 
+            // btnToLower
+            // 
+            this.btnToLower.Location = new System.Drawing.Point(132, 132);
+            this.btnToLower.Name = "btnToLower";
+            this.btnToLower.Size = new System.Drawing.Size(110, 41);
+            this.btnToLower.TabIndex = 9;
+            this.btnToLower.Text = ".ToLower()";
+            this.btnToLower.UseVisualStyleBackColor = true;
+            // 
+            // btnReplace
+            // 
+            this.btnReplace.Location = new System.Drawing.Point(248, 132);
+            this.btnReplace.Name = "btnReplace";
+            this.btnReplace.Size = new System.Drawing.Size(110, 41);
+            this.btnReplace.TabIndex = 10;
+            this.btnReplace.Text = ".Replace";
+            this.btnReplace.UseVisualStyleBackColor = true;
+            this.btnReplace.Click += new System.EventHandler(this.btnReplace_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnReplace);
+            this.groupBox1.Controls.Add(this.btnToLower);
+            this.groupBox1.Controls.Add(this.btnToUpper);
+            this.groupBox1.Controls.Add(this.btnIndexof);
+            this.groupBox1.Controls.Add(this.btnEndsWith);
+            this.groupBox1.Controls.Add(this.btnStartsWith);
+            this.groupBox1.Controls.Add(this.btnSubstring35);
+            this.groupBox1.Controls.Add(this.btnSubstring4);
+            this.groupBox1.Controls.Add(this.btnLength);
+            this.groupBox1.Controls.Add(this.txtOutput);
+            this.groupBox1.Controls.Add(this.txtInput);
+            this.groupBox1.Location = new System.Drawing.Point(2, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(384, 269);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // btnRegular
+            // 
+            this.btnRegular.Location = new System.Drawing.Point(12, 306);
+            this.btnRegular.Name = "btnRegular";
+            this.btnRegular.Size = new System.Drawing.Size(75, 23);
+            this.btnRegular.TabIndex = 12;
+            this.btnRegular.Text = "Regular";
+            this.btnRegular.UseVisualStyleBackColor = true;
+            this.btnRegular.Click += new System.EventHandler(this.btnRegular_Click);
+            // 
+            // lstOutput
+            // 
+            this.lstOutput.FormattingEnabled = true;
+            this.lstOutput.Location = new System.Drawing.Point(117, 287);
+            this.lstOutput.Name = "lstOutput";
+            this.lstOutput.ScrollAlwaysVisible = true;
+            this.lstOutput.Size = new System.Drawing.Size(212, 238);
+            this.lstOutput.TabIndex = 13;
+            // 
+            // btnReverse
+            // 
+            this.btnReverse.Location = new System.Drawing.Point(12, 354);
+            this.btnReverse.Name = "btnReverse";
+            this.btnReverse.Size = new System.Drawing.Size(75, 23);
+            this.btnReverse.TabIndex = 14;
+            this.btnReverse.Text = "Reverse";
+            this.btnReverse.UseVisualStyleBackColor = true;
+            this.btnReverse.Click += new System.EventHandler(this.btnReverse_Click);
+            // 
+            // btnLetter
+            // 
+            this.btnLetter.Location = new System.Drawing.Point(12, 404);
+            this.btnLetter.Name = "btnLetter";
+            this.btnLetter.Size = new System.Drawing.Size(75, 23);
+            this.btnLetter.TabIndex = 15;
+            this.btnLetter.Text = "Letter \"a\"";
+            this.btnLetter.UseVisualStyleBackColor = true;
+            this.btnLetter.Click += new System.EventHandler(this.btnLetter_Click);
+            // 
+            // btnProblem1
+            // 
+            this.btnProblem1.Location = new System.Drawing.Point(53, 570);
+            this.btnProblem1.Name = "btnProblem1";
+            this.btnProblem1.Size = new System.Drawing.Size(75, 23);
+            this.btnProblem1.TabIndex = 16;
+            this.btnProblem1.Text = "Vowel count";
+            this.btnProblem1.UseVisualStyleBackColor = true;
+            this.btnProblem1.Click += new System.EventHandler(this.btnProblem1_Click);
+            // 
+            // btnProblem2
+            // 
+            this.btnProblem2.Location = new System.Drawing.Point(146, 570);
+            this.btnProblem2.Name = "btnProblem2";
+            this.btnProblem2.Size = new System.Drawing.Size(75, 23);
+            this.btnProblem2.TabIndex = 17;
+            this.btnProblem2.Text = "button1";
+            this.btnProblem2.UseVisualStyleBackColor = true;
+            this.btnProblem2.Click += new System.EventHandler(this.btnProblem2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(715, 649);
+            this.Controls.Add(this.btnProblem2);
+            this.Controls.Add(this.btnProblem1);
+            this.Controls.Add(this.btnLetter);
+            this.Controls.Add(this.btnReverse);
+            this.Controls.Add(this.lstOutput);
+            this.Controls.Add(this.btnRegular);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button btnLength;
+        private System.Windows.Forms.Button btnSubstring4;
+        private System.Windows.Forms.Button btnSubstring35;
+        private System.Windows.Forms.Button btnStartsWith;
+        private System.Windows.Forms.Button btnEndsWith;
+        private System.Windows.Forms.Button btnIndexof;
+        private System.Windows.Forms.Button btnToUpper;
+        private System.Windows.Forms.Button btnToLower;
+        private System.Windows.Forms.Button btnReplace;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnRegular;
+        private System.Windows.Forms.ListBox lstOutput;
+        private System.Windows.Forms.Button btnReverse;
+        private System.Windows.Forms.Button btnLetter;
+        private System.Windows.Forms.Button btnProblem1;
+        private System.Windows.Forms.Button btnProblem2;
+    }
+}
+
---
+++ 02-Introduction_To_Strings/Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace _02_Introduction_To_Strings
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string input;
+        #region Intro
+        private void btnLength_Click(object sender, EventArgs e)
+        {
+            input = txtInput.Text;
+            if (input != string.Empty)
+            {
+                txtOutput.Text = input.Length.ToString();
+            }
+        }
+
+        private void btnSubstring4_Click(object sender, EventArgs e)
+        {
+            input = txtInput.Text;
+            if (input != string.Empty)
+            {
+                txtOutput.Text = input.Substring(4);
+            }
+        }
+
+        private void btnSubstring35_Click(object sender, EventArgs e)
+        {
+            input = txtInput.Text;
+            if (input != string.Empty)
+            {
+                txtOutput.Text = input.Substring(2, 5);
+            }
+        }
+
+        private void btnStartsWith_Click(object sender, EventArgs e)
+        {
+            input = txtInput.Text;
+            if (input != string.Empty)
+            {
+                txtOutput.Text = input.StartsWith("S").ToString();
+            }
+        }
+
+        private void btnIndexof_Click(object sender, EventArgs e)
+        {
+            input = txtInput.Text;
+            if (input != string.Empty)
+            {
+                txtOutput.Text = input.IndexOf("A").ToString();
+            }
+        }
+
+        private void btnToUpper_Click(object sender, EventArgs e)
+        {
+            input = txtInput.Text;
+            if (input != string.Empty)
+            {
+                txtOutput.Text = input.ToUpper();
+            }
+        }
+
+        private void btnReplace_Click(object sender, EventArgs e)
+        {
+            input = txtInput.Text;
+            if (input != string.Empty)
+            {
+                txtOutput.Text = input.Replace("a", "x");
+            }
+        }
+        #endregion
+        #region ListBox Example
+        private void btnRegular_Click(object sender, EventArgs e)
+        {
+            string x;
+            x = Microsoft.VisualBasic.Interaction.InputBox("Enter", "Strings", "", 200, 200);
+            for (int i = 0; i < x.Length; i++)
+            {
+                lstOutput.Items.Add(x.Substring(i, 1) + Environment.NewLine);
+            }
+
+
+        }
+        private void btnReverse_Click(object sender, EventArgs e)
+        {
+            string x;
+            x = Microsoft.VisualBasic.Interaction.InputBox("Enter", "Strings", "", 200, 200);
+            for (int i = x.Length - 1; i >= 0; i--)
+            {
+                lstOutput.Items.Add(x.Substring(i, 1) + Environment.NewLine);
+            }
+        }
+        private void btnLetter_Click(object sender, EventArgs e)
+        {
+            //user input
+            string x;
+            //letter that we want to keep a count on
+            string letter = "a";
+            //variable to keep the count
+            int a = 0;
+            x = Microsoft.VisualBasic.Interaction.InputBox("Enter", "Strings", "", 200, 200);
+            //one way
+            for (int i = 0; i < x.Length; i++)
+            {
+                if (x.Substring(i, 1) == letter)
+                {
+                    a++;
+                }
+            }
+            MessageBox.Show(a.ToString());
+
+            //another way -> this is harder...
+            //we can have a list of letters
+            //List<string> letters = new List<string>();
+            //foreach (var s in x)
+            //{
+            //    letters.Add(s.ToString());
+            //}
+            //foreach (string s in letters)
+            //{
+            //    lstOutput.Items.Add(s);
+            //    if (s == letter)
+            //        a++;
+            //}
+            //MessageBox.Show(a.ToString());
+
+        }
+        #endregion
+        #region Practice Problems
+        private void btnProblem1_Click(object sender, EventArgs e)
+        {
+            //store vowels in an array
+            string[] vowels = new[] { "a", "e", "i", "o", "u" };
+            //get the input from the user
+            string input = Microsoft.VisualBasic.Interaction.InputBox("Enter", "Strings", "", 200, 200);
+            //this is to count the vowels 
+            int a = 0;
+            foreach (string s in vowels) //foreach vowel in our array
+            {
+                //if (input.Contains(s))//see if our user input has any of them and if so count
+                //    a++; // this doesnt work as its not counting vowels that are being used twice
+                for (int i = 0; i < input.Length; i++)
+                {
+                    string x = input.Substring(i, 1);//see if our user input has any of them and if so count
+                    if (x.Contains(s))
+                    { a++; }
+                }
+
+            }
+            MessageBox.Show(a.ToString());
+
+
+        }
+        #endregion
+
+        private void btnProblem2_Click(object sender, EventArgs e)
+        {
+            //write a program that accept a sentence as data and  will determine the number of times the word "the" appears in it
+            //user input
+            string input = Microsoft.VisualBasic.Interaction.InputBox("Enter", "Strings", "", 200, 200);
+            string the = "the";
+            string space = " ";
+            int a = 0;
+            int index = 0;
+
+            //break up the words by space
+            char separator = ' ';
+            //store the words into an array
+            string[] words = input.Split(separator);
+            foreach(string word in words)
+            {
+                if (word == the) 
+                { a++; }
+
+            }
+            MessageBox.Show(a.ToString());
+
+
+
+
+        }
+    }
+}
